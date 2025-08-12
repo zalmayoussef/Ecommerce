fd9fa20080791d61311b10ad36a8d23ead7dd725
Salma <zalmayoussief@gmail.com>
2025-08-12 16:45:30 +0300

    ExceptionAdvice for OrderController

---
+++ src/main/java/com/example/ecommerce/exception/ExceptionAdvice.java
@@ -0,0 +1,14 @@
+package com.example.ecommerce.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionAdvice {
+    @ExceptionHandler(OrderNotFoundException.class)
+    public ResponseEntity<String> handleOrderNotFound(OrderNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+}
