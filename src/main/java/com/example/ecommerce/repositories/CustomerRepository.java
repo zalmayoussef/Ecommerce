dd54a9777615a0adb3408e69d2be7b7f1dfde35b
Salma <zalmayoussief@gmail.com>
2025-08-13 23:10:49 +0300

    findByEmail added

--- src/main/java/com/example/ecommerce/repositories/CustomerRepository.java
+++ src/main/java/com/example/ecommerce/repositories/CustomerRepository.java
@@ -3,6 +3,9 @@ package com.example.ecommerce.repositories;
 import com.example.ecommerce.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByEmail(String email);
 
 }
